a5d34c186e8dc1fdfe551655f82b9bb2cccd0efb
Manasvi Narayanan <mnaray5@uic.edu>
2022-02-02 10:20:52 -0600

    made a puddle class

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 
 d = 10;
 fade = 255;
+xClick = yClick = 0;
+z = 0;
+var puddles = [];
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -10,20 +14,31 @@ function setup() {
 
 function draw() {
   
-  background("#CBF8FF");
+  background("#285988");
   noFill();
-  stroke(45,49,161,fade);
+  stroke(175,205,234,fade);
   strokeWeight(3);
   circle(300,200,d);
-
-  circle(450,600,d);
+  circle(900,650,d);
+  circle(xClick,yClick,d);
+  //circle(random(windowWidth),random(windowHeight),d);
 
   d = d+10;
   fade = fade-5;
 
+  if(fade <=0){
+    fade = 255;
+    d = 10;
+  }
+
   
 }
 
+function makeRandomPuddles(){
+  let p = new Puddle(random(100, windowWidth-50), random(100, windowHeight-50),0);
+  puddles.push(p);
+  puddles.add = 5;
+}
 
 function setLineDash(list) {
   drawingContext.setLineDash(list);
@@ -32,6 +47,22 @@ function setLineDash(list) {
 function mousePressed(){
     xClick = mouseX;
     yClick = mouseY;
-    console.log("(" + xClick + ", " + yClick + ")");
+    z = 1;
+    
+}
+
+
+class Puddle{
+  constructor(x,y, opt) {
+    this.x = x;
+    this.y = y;
+    this.d = 10;
+    this.option = opt;
+    if(opt != 0){
+        this.fade = 300;
+    } else {
+        this.fade = 150;
+    }
+  }
 }
 
